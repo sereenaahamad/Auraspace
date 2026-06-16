66e346a810cbd4bafa3f23cad37bc9ccfa98a21d
sereenaahamad <sereenaahamed@gmail.com>
2026-06-16 11:29:30 +0530

    Add files via upload

--- src/data/moods.js
+++ src/data/moods.js
@@ -1,8 +1,8 @@
 export const moods = [
-  { name: "Stressed", color: "#4A90E2", music: "/music/calm.mp3", video: "/video/calm.mp4" },
-  { name: "Sad", color: "#1C1C1C", music: "/music/sad.mp3", video: "/video/sad.mp4"  },
-  { name: "Frustrated", color: "#FFD93D", music: "/music/frustrated.mp3", video: "/video/frustrated.mp4" },
-  { name: "Depressed", color: "#A18AFF", music: "/music/depressed.mp3", video: "/video/depressed.mp4" },
-  { name: "Lonely", color: "#3A3A5A", music: "/music/lonely.mp3", video: "/video/lonely.mp4" },
-  { name: "Doubt", color: "#FF4D6D", music: "/music/doubt.mp3", video: "/video/doubt.mp4" },
+  { name: "Stressed", color: "#4A90E2", music: "/music/calm.mp3", video: "https://pub-eb4db51586d94241ae535594e7686b05.r2.dev/calm.mp4" },
+  { name: "Sad", color: "#1C1C1C", music: "/music/sad.mp3", video: "https://pub-eb4db51586d94241ae535594e7686b05.r2.dev/sad.mp4"  },
+  { name: "Frustrated", color: "#FFD93D", music: "/music/frustrated.mp3", video: "https://pub-eb4db51586d94241ae535594e7686b05.r2.dev/frustrated.mp4" },
+  { name: "Depressed", color: "#A18AFF", music: "/music/depressed.mp3", video: "https://pub-eb4db51586d94241ae535594e7686b05.r2.dev/depressed.mp4" },
+  { name: "Lonely", color: "#3A3A5A", music: "/music/lonely.mp3", video: "https://pub-eb4db51586d94241ae535594e7686b05.r2.dev/lonely.mp4" },
+  { name: "Doubt", color: "#FF4D6D", music: "/music/doubt.mp3", video: "https://pub-eb4db51586d94241ae535594e7686b05.r2.dev/doubt.mp4" },
 ];
